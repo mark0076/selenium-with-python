712d3bac8fa4df5cb92e3dd3ded160dc21766dfc
mark0076 <rma0076@gmail.com>
2019-10-14 02:14:35 +0300

    created new repository and added my page object project

--- lang/test_items.py
+++ lang/test_items.py
@@ -3,6 +3,6 @@ import time
 link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 def test_check_the_existence_of_add_to_bastet_button(browser):
     browser.get(link)
-    #time.sleep(30)
+    time.sleep(30)
     button = browser.find_element_by_css_selector("button.btn.btn-lg.btn-primary.btn-add-to-basket")
     assert button,"Button is not found"
